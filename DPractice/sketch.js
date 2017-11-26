ea1ff0dd7078a90e2d96cd00133d97b27714e96e
TDean505 <32494980+TDean505@users.noreply.github.com>
2017-11-26 14:04:15 -0800

    Update sketch.js

--- DPractice/sketch.js
+++ DPractice/sketch.js
@@ -11,13 +11,12 @@ var diagram = { //object for animation
 
 function setup() {
 presetup();
-//home();
- // this is the home of the websight, takes back to start portal
+
 }
 
 function draw(){
 
-//home();
+
 }
 function home(){ // this is where you add things to the first page. will allow user to naviagate back to default
   removeElements();
@@ -33,11 +32,15 @@ function basicbutton(x,y,z,a){ //x=x y=y z=name of button  a = function name you
   button = createButton(z);
   button.position(x, y);
   button.mousePressed(a); //use this comant for function actions for button
+  button.style("background-color", "#696969");
+  button.style("border","0px");
 }
 
 function basicbuttona(x,y,z,a,b){ //x=x y=y z=name of button  a = function name you want to have happen b= second funciton
   button = createButton(z);
   button.position(x, y);
+  button.style("background-color", "#696969");
+  button.style("border","0px");
   button.mousePressed(a);
   button.mousePressed(b);
 }
@@ -52,6 +55,7 @@ removeElements();
 Imge(0,0,'backroundinfoa.jpg',windowWidth, windowHeight);
 backgroundinfotext();
 basicbutton(50,100,"home page", home);
+backroundinfocontent();
 
 }
 
@@ -59,6 +63,7 @@ function learningaboutimplantsF(){ //controls what happens when learning about i
 removeElements();
 nearondiagram();
 learningaboutimplantstext();
+learningaboutimplantscontent();
 basicbuttona(50,50,"home page",cthing, home);
 basicbutton(100,150,"fire neuron", animation);
 basicbutton(235,150,"deactivate neuron", deactivateneuron);
@@ -68,7 +73,9 @@ basicbutton(235,150,"deactivate neuron", deactivateneuron);
 function thefuture(){ // controls what happens when button "the future" is pressed
 removeElements();
 Imge(0,0,'thefutureimg.jpg', windowWidth, windowHeight);
-basicbutton(100,50,"home page", home);
+thefuturecontent();
+basicbutton(100,100,"home page", home);
+thefuturetext();
 }
 
 function frontpagetext(){ // title text for front page
@@ -164,9 +171,52 @@ function backgroundinfotext(){ //backround info title
   myDiv.style("font-size","55px");
 }
 
+function thefuturetext(){
+  var myDiv = createDiv("The Future of Brain Implants and VR");
+  myDiv.style("position", 15, 5);
+  //myDiv.style("rotate", 45);
+  myDiv.style("color", "#696969");
+  myDiv.style("font-size","55px");
+
+}
+
+function backroundinfocontent(){
+  var myDiv = createDiv("Virtual Reality is a system which allows a human, or multiple humans, to interact with a virtual world. The user of the virtual reality system uses their body to move throughout and interact with the virtual environment. The most successful virtual reality systems are capable of making the user feel as though they are truly present in the virtual world, with all their senses being artificially stimulated to trick the brain into believing the body is present in the virtual world. The virtual environment must be updated everytime the user moves, so initially virtual reality machines were very expensive and only used in high budget projects. However, the cost of virtual reality machines has dropped significantly and has been applied to other fields, like gaming. A traditional VR setup uses a headset to show the user the virtual world and hand controllers or gloves to allow the user to interact with the virtual world. Motion sensors are also utilized to allow the user to move around in the virtual world.");
+  myDiv.style("position", 15, 200);
+  //myDiv.style("rotate", 45);
+  myDiv.style("color", "#F8F8FF");
+  myDiv.style("font-size","14px");
+
+}
+
+function learningaboutimplantscontent(){
+  var myDiv = createDiv("Brain implants work by tracking individual neuron action and using the activation of a neuron as a command to perform a function. A brain implant must be programmed for which neurons trigger specific motions, something that a human must do by experimenting on which neurons control specific actions. Brain implants work by using very small wires that are inserted into the brain. These wires are very sensitive and can sense the activation and deactivation of neurons. ");
+  myDiv.style("position", 510, 100);
+  //myDiv.style("rotate", 45);
+  myDiv.style("color", "#F8F8FF");
+  myDiv.style("font-size","14px");
+
+}
+
+function thefuturecontent(){
+  var myDiv = createDiv("The future will bring brain implants and VR together. Brain implants will be able to not only respond to the users’ commands but stimulate the neurons as well to create a completely immersive VR experience. The result will be an environment where the user simply uses their mind to interact with the virtual world.");
+  myDiv.style("position", 50, 200);
+  //myDiv.style("rotate", 45);
+  myDiv.style("color", "#696969");
+  myDiv.style("font-size","20px");
+
+}
+
+
 function presetup(){
   createCanvas(windowWidth, windowHeight);
   background(100);
   basicbuttona(windowWidth/2, windowHeight/2,"Brain Implants and VR Web App",cthing, home);
+  var myDiv = createDiv("CLICK THE BUTTON!!!");
+  myDiv.style("position", 50, 200);
+  //myDiv.style("rotate", 45);
+  myDiv.style("color", "#F8F8FF");
+  myDiv.style("font-size","70px");
 
 }
+
